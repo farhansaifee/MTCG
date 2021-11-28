e7f16431659ddf2229c96b68d9b07ad97fb9b69d
farhansaifee <farhan.saifee11@gmail.com>
2021-11-28 02:32:55 +0100

    Created HTTPServer & HTTPProcessor class in Project

---
+++ CODE/MonsterCardGame/MonsterCardGame/HttpProcessor.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+
+namespace MonsterCardGame
+{
+    class HttpProcessor
+    {
+        private TcpClient socket;
+        private HttpServer httpServer;
+
+        public string Method { get; private set; }
+        public string Path { get; private set; }
+        public string Version { get; private set; }
+
+        public Dictionary<string, string> Headers { get; }
+
+        public HttpProcessor(TcpClient s, HttpServer httpServer)
+        {
+            this.socket = s;
+            this.httpServer = httpServer;
+
+            Method = null;
+            Headers = new();
+        }
+
+        public void Process()
+        {
+            var writer = new StreamWriter(socket.GetStream()) { AutoFlush = true };
+            var reader = new StreamReader(socket.GetStream());
+            Console.WriteLine();
+
+            // read (and handle) the full HTTP-request
+            string line = null;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+                if (line.Length == 0)
+                    break;  // empty line means next comes the content (which is currently skipped)
+
+                // handle first line of HTTP
+                if (Method == null)
+                {
+                    var parts = line.Split(' ');
+                    Method = parts[0];
+                    Path = parts[1];
+                    Version = parts[2];
+                }
+                // handle HTTP headers
+                else
+                {
+                    var parts = line.Split(": ");
+                    Headers.Add(parts[0], parts[1]);
+                }
+            }
+
+            // write the full HTTP-response
+            string content = $"<html><body><h1>test server</h1>" +
+                $"Current Time: {DateTime.Now}" +
+                $"<form method=\"GET\" action=\"/form\">" +
+                $"<input type=\"text\" name=\"foo\" value=\"foovalue\">" +
+                $"<input type=\"submit\" name=\"bar\" value=\"barvalue\">" +
+                $"</form></html>";
+
+            Console.WriteLine();
+            WriteLine(writer, "HTTP/1.1 200 OK");
+            WriteLine(writer, "Server: My simple HttpServer");
+            WriteLine(writer, $"Current Time: {DateTime.Now}");
+            WriteLine(writer, $"Content-Length: {content.Length}");
+            WriteLine(writer, "Content-Type: text/html; charset=utf-8");
+            WriteLine(writer, "");
+            WriteLine(writer, content);
+
+            writer.WriteLine();
+            writer.Flush();
+            writer.Close();
+        }
+
+        private void WriteLine(StreamWriter writer, string s)
+        {
+            Console.WriteLine(s);
+            writer.WriteLine(s);
+        }
+    }
+}
---
+++ CODE/MonsterCardGame/MonsterCardGame/HttpServer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MonsterCardGame
+{
+    class HttpServer
+    {
+
+        protected int port;
+        TcpListener listener;
+
+        public HttpServer(int port)
+        {
+            this.port = port;
+        }
+
+        public void Run()
+        {
+            listener = new TcpListener(IPAddress.Loopback, port);
+            listener.Start(5);
+            while (true)
+            {
+                TcpClient s = listener.AcceptTcpClient();
+                HttpProcessor processor = new HttpProcessor(s, this);
+                new Thread(processor.Process).Start();
+                Thread.Sleep(1);
+            }
+        }
+
+    }
+}
