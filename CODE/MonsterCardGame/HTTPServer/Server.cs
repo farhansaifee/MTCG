6df5699fdbeabeb677690ac6aaa946a9215b50e7
Mohammad Farhan Saifee <farhan.saifee11@gmail.com>
2021-11-29 20:41:06 +0100

    Insomnia/Postman Research + Started Server for Project

---
+++ CODE/MonsterCardGame/HTTPServer/Server.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace HTTPServer
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+
+            TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 8080);
+
+
+        }
+    }
+}
