75b243672e7cfbcba3dbdd49427f68b8a7e271b9
Mohammad Farhan Saifee <farhan.saifee11@gmail.com>
2021-12-01 18:46:12 +0100

    User Class erweitert mit Methoden, Setter/Getter erstellt; Serialization

--- CODE/MonsterCardGame/MonsterCardGame/Program.cs
+++ CODE/MonsterCardGame/MonsterCardGame/Program.cs
@@ -6,7 +6,7 @@ namespace MonsterCardGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
--- CODE/MonsterCardGame/MonsterCardGame/User.cs
+++ CODE/MonsterCardGame/MonsterCardGame/User.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,9 +11,45 @@ namespace MonsterCardGame
     class User
     {
 
-        public string username;
-        private string password;
-        private int coins;
+        public string id { get; set; }
+        public string username { get; set; }
+        private string password { get; set; }
+        private int coins { get; set; }
+        public int score { get; set; }
+
+        public void register()
+        {
+            Console.WriteLine("User is registered!");
+        }
+        public void login()
+        {
+            Console.WriteLine("User is logged in!");
+        }
+
+
+        //Get User Information
+        public static async Task Main()
+        {
+            using HttpClient client = new()
+            {
+                BaseAddress = new Uri("https://localhost:8080")
+            };
+
+            //HttpClient client2 = new HttpClient();
+            //client2.BaseAddress.AbsolutePath.Contains("https://localhost:8080");
+
+            // Get the user information.
+            User user = await client.GetFromJsonAsync<User>("users/1");
+            Console.WriteLine($"Id: {user.id}");
+            Console.WriteLine($"Username: {user.username}");
+            Console.WriteLine($"Username: {user.password}");
+            Console.WriteLine($"Username: {user.coins}");
+            Console.WriteLine($"Username: {user.score}");
+
+            // Post a new user.
+            HttpResponseMessage response = await client.PostAsJsonAsync("users", user);
+            Console.WriteLine($"{(response.IsSuccessStatusCode ? "Success" : "Error")} - {response.StatusCode}");
+        }
 
     }
 }
