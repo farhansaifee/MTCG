4a688b57ea30663d28526f3b5d343f57648e232f
Mohammad Farhan Saifee <farhan.saifee11@gmail.com>
2021-12-02 21:42:18 +0100

    Started to connect to DB; Installed NPGSQL in VS2019

--- CODE/MonsterCardGame/MonsterCardGame/Database.cs
+++ CODE/MonsterCardGame/MonsterCardGame/Database.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Npgsql;
 
 namespace MonsterCardGame
 {
     class Database
     {
 
-
+        private static NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(@"server=localhost;Port=5050");
+        }
 
     }
 }
