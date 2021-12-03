bf0d714230ed9c5190d8863209e5d5769ff370a1
Mohammad Farhan Saifee <farhan.saifee11@gmail.com>
2021-12-03 22:40:20 +0100

    Server tried to create, doesn't work properly (need to change it)

--- CODE/MonsterCardGame/MonsterCardGame/Server/Server.cs
+++ CODE/MonsterCardGame/MonsterCardGame/Server/Server.cs
@@ -14,43 +14,79 @@ namespace MonsterCardGame.Server
     {
 
         // Create new HTTP Listener Instance/Object
-        static TcpListener listener;
+        static TcpListener server = null;
         
 
         static void Main(string[] args)
         {
+            
             try
             {
+                // Set the TcpListener on port 5050.
+                Int32 port = 8080;
+                IPAddress localhost = IPAddress.Parse("127.0.0.1");
 
-                string localhost = "127.0.0.1";
-                int port = 8080;
-                listener = new TcpListener(IPAddress.Parse(localhost), port);
-                listener.Start(); // Start TcpListener
-                Console.WriteLine("Server started!");
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localhost, port);
 
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[1024];
+                String data = null;
+
+                // Enter the listening loop.
                 while (true)
                 {
-                    Console.WriteLine("Waiting for connection...");
-                    TcpClient client = listener.AcceptTcpClient(); // Clients connect to Server
-                    Console.WriteLine("Accepted new Client.");
-                    StreamReader reader = new StreamReader(client.GetStream());
-                    StreamWriter writer = new StreamWriter(client.GetStream());
-                    string s = string.Empty;
-                    while ((s != reader.ReadLine()).Equals("Exit") || (s != null))
+                    Console.Write("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests.
+                    // You could also use server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    data = null;
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    // Loop to receive all the data sent by the client.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
                     {
-                        Console.WriteLine("From Client: " + s);
-                        writer.WriteLine("From Server: " + s);
-                        writer.Flush();
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+
+                        // Process the data sent by the client.
+                        data = data.ToUpper();
+
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("Sent: {0}", data);
                     }
 
+                    // Shutdown and end connection
+                    client.Close();
                 }
-
             }
-            catch(Exception e)
+            catch (SocketException e)
             {
-                Console.WriteLine(e);
+                Console.WriteLine("SocketException: {0}", e);
             }
-            
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+
 
         }
 
