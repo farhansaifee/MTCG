6c82fa566911ac286e4321a21154b9a11cdd0b96
Mohammad Farhan Saifee <farhan.saifee11@gmail.com>
2021-12-04 13:50:44 +0100

    Pushed changes from server class

--- CODE/MonsterCardGame/MonsterCardGame/Server/Server.cs
+++ CODE/MonsterCardGame/MonsterCardGame/Server/Server.cs
@@ -57,17 +57,17 @@ namespace MonsterCardGame.Server
                     while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
                     {
                         // Translate data bytes to a ASCII string.
-                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                        Console.WriteLine("Received: {0}", data);
+                        //data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        //Console.WriteLine("Received: {0}", data);
 
                         // Process the data sent by the client.
-                        data = data.ToUpper();
+                        //data = data.ToUpper();
 
-                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+                        //byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
 
                         // Send back a response.
-                        stream.Write(msg, 0, msg.Length);
-                        Console.WriteLine("Sent: {0}", data);
+                        //stream.Write(msg, 0, msg.Length);
+                        //Console.WriteLine("Sent: {0}", data);
                     }
 
                     // Shutdown and end connection
