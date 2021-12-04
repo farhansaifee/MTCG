9dd13041b326fa8319b56012d5574404d10cdf2d
Mohammad Farhan Saifee <farhan.saifee11@gmail.com>
2021-12-04 14:57:56 +0100

    solved Merge Conflicts

--- CODE/MonsterCardGame/MonsterCardGame/Server/Server.cs
+++ CODE/MonsterCardGame/MonsterCardGame/Server/Server.cs
@@ -14,7 +14,7 @@ namespace MonsterCardGame.Server
     {
 
         // Create new HTTP Listener Instance/Object
-<<<<<<< HEAD
+
         static HttpListener listener = new HttpListener();
 
         static void Main(string[] args)
@@ -26,81 +26,6 @@ namespace MonsterCardGame.Server
             Console.WriteLine("Server started.");
             Thread responseThread = new Thread(GetResponse);
             responseThread.Start(); // start the response thread
-=======
-        static TcpListener server = null;
-        
-
-        static void Main(string[] args)
-        {
-            
-            try
-            {
-                // Set the TcpListener on port 5050.
-                Int32 port = 8080;
-                IPAddress localhost = IPAddress.Parse("127.0.0.1");
-
-                // TcpListener server = new TcpListener(port);
-                server = new TcpListener(localhost, port);
-
-                // Start listening for client requests.
-                server.Start();
-
-                // Buffer for reading data
-                Byte[] bytes = new Byte[1024];
-                String data = null;
-
-                // Enter the listening loop.
-                while (true)
-                {
-                    Console.Write("Waiting for a connection... ");
-
-                    // Perform a blocking call to accept requests.
-                    // You could also use server.AcceptSocket() here.
-                    TcpClient client = server.AcceptTcpClient();
-                    Console.WriteLine("Connected!");
-
-                    data = null;
-
-                    // Get a stream object for reading and writing
-                    NetworkStream stream = client.GetStream();
-
-                    int i;
-
-                    // Loop to receive all the data sent by the client.
-                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-                    {
-                        // Translate data bytes to a ASCII string.
-                        //data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                        //Console.WriteLine("Received: {0}", data);
-
-                        // Process the data sent by the client.
-                        //data = data.ToUpper();
-
-                        //byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-
-                        // Send back a response.
-                        //stream.Write(msg, 0, msg.Length);
-                        //Console.WriteLine("Sent: {0}", data);
-                    }
-
-                    // Shutdown and end connection
-                    client.Close();
-                }
-            }
-            catch (SocketException e)
-            {
-                Console.WriteLine("SocketException: {0}", e);
-            }
-            finally
-            {
-                // Stop listening for new clients.
-                server.Stop();
-            }
-
-            Console.WriteLine("\nHit enter to continue...");
-            Console.Read();
-
->>>>>>> 6c82fa566911ac286e4321a21154b9a11cdd0b96
 
         }
 
